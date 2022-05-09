775f0c753ffa0620eeea4ffb0d5a18ca52664b85
iggy <allyson.nostrandd@gmail.com>
2022-05-09 14:25:00 -0700

    defined our models, added relationships in index.js

--- models/Topic.js
+++ models/Topic.js
@@ -1,1 +1,44 @@
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Topic extends Model {}
+
+Topic.init({
+    
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+
+    text: {
+        type: DataTypes.text,
+        allowNull: false  
+    },
+
+    category_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Category',
+            key: 'id'
+        }
+    }
+},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'topic',
+  }
+);
+
+module.exports = Topic;
--- models/User.js
+++ models/User.js
@@ -1,1 +1,49 @@
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+const bcrypt = require("bcrypt")
+
+class User extends Model {}
+
+User.init({
+    
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        Unique: true
+    },
+
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len:[8]
+        }
+    }, 
+
+    creation_date: {
+        type: DataTypes.DATE,
+    }
+},
+  {
+    hooks:{
+        beforeCreate:async userdata=>{
+            userdata.password = await bcrypt.hash(userdata.password,5)
+            return userdata
+        }},
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
