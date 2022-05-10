56c7a224f3437a3ae811402bd9151b14222ec0c4
iggy <allyson.nostrandd@gmail.com>
2022-05-10 14:53:44 -0700

    added login js code

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+document.querySelector("#login").addEventListener("submit",e=>{
+    e.preventDefault();
+    const userObj = {
+        username:document.querySelector("#loginUsername").value,
+        password:document.querySelector("#loginPassword").value,
+    }
+    console.log(userObj)
+    fetch("/api/users/login",{
+        method:"POST",
+        body:JSON.stringify(userObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+            location.href="/profile"
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
